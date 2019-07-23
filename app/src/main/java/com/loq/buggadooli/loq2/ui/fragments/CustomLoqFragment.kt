be2196f9812ec89e457bc88f16a12479e42da70c
DerrickRocha <drocha616@gmail.com>
2019-07-22 22:01:27 -0600

    Fixed margins and padding in item_custom_loq

--- app/src/main/java/com/loq/buggadooli/loq2/ui/fragments/CustomLoqFragment.kt
+++ app/src/main/java/com/loq/buggadooli/loq2/ui/fragments/CustomLoqFragment.kt
@@ -1,5 +1,7 @@
 package com.loq.buggadooli.loq2.ui.fragments
 
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.loq.buggadooli.loq2.R
 import com.loq.buggadooli.loq2.constants.Constants
 import com.loq.buggadooli.loq2.extensions.*
@@ -37,6 +40,12 @@ class CustomLoqFragment: Fragment() {
         loqAdapter.checkboxListener = checkboxListener
         recycler.apply {
             layoutManager = LinearLayoutManager(safeActivity)
+            addItemDecoration(object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+                    super.getItemOffsets(outRect, view, parent, state)
+                    outRect.bottom = 16
+                }
+            })
             adapter = loqAdapter
         }
         recycler.addOnItemClick { position, view ->
