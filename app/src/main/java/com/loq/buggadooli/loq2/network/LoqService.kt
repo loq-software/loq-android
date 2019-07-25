8d44a9905c6c0179642a0495e28ed37258eae65f
DerrickRocha <drocha616@gmail.com>
2019-07-24 22:49:14 -0600

    Code cleanup. Loq Service.

---
+++ app/src/main/java/com/loq/buggadooli/loq2/network/LoqService.kt
@@ -0,0 +1,26 @@
+package com.loq.buggadooli.loq2.network
+
+import com.loq.buggadooli.loq2.models.BlockedApplication
+import io.reactivex.Observable
+
+interface LoqService{
+
+    fun getLoqs(): Observable<BlockedApplication>
+
+    fun addLoq(loq: BlockedApplication): Observable<BlockedApplication>
+}
+
+class RealLoqService: LoqService {
+
+    override fun getLoqs(): Observable<BlockedApplication> {
+        return Observable.create { emitter ->
+
+        }
+    }
+
+    override fun addLoq(loq: BlockedApplication): Observable<BlockedApplication> {
+        return Observable.create { emitter ->
+
+        }
+    }
+}
