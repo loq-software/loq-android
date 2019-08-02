532923ec88989bf27934f3d87bb4c103e074f4e1
Derrick Rocha <drocha616@gmail.com>
2019-08-01 21:37:18 -0600

    Using rxjava for polling mechanism. (#25)

--- app/src/main/java/com/easystreetinteractive/loq/loqer/LoqerManager.kt
+++ app/src/main/java/com/easystreetinteractive/loq/loqer/LoqerManager.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.CountDownTimer
 import androidx.lifecycle.LifecycleOwner
 import com.easystreetinteractive.loq.extensions.attachLifecycle
-import com.easystreetinteractive.loq.extensions.ioToMain
 import com.easystreetinteractive.loq.extensions.isLocked
 import com.easystreetinteractive.loq.extensions.subscribeForOutcome
 import com.easystreetinteractive.loq.models.Loq
@@ -15,6 +14,9 @@ import com.easystreetinteractive.loq.network.api.AuthenticationService
 import com.easystreetinteractive.loq.network.api.LoqService
 import com.easystreetinteractive.loq.repositories.ApplicationsRepository
 import com.easystreetinteractive.loq.ui.activities.LockScreenActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import java.util.ArrayList
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -56,25 +58,27 @@ class RealLoqerManager(
     }
 
     override fun scheduleMethod(service: Service,owner: LifecycleOwner) {
-        scheduler.scheduleAtFixedRate(Runnable {
-            val userId = authenticationService.getCurrentUser()?.uid?: return@Runnable
-            loqService.getLoqs(userId)
-                    .ioToMain()
-                    .subscribeForOutcome { outcome ->
-                        when(outcome){
-                            is Outcome.Success -> {
-                                val loqs = outcome.data
-                                val activityOnTop = applicationsRepository.getForegroundApp()
-                                if (loqs.isLocked(activityOnTop)){
-                                    val dialogIntent = Intent(service, LockScreenActivity::class.java)
-                                    dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                    service.startActivity(dialogIntent)
-                                }
+
+        Observable.interval(0, 10, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .switchMap {
+                    loqService.getLoqs(authenticationService.getCurrentUser()?.uid?: "")
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeForOutcome { outcome ->
+                    when(outcome){
+                        is Outcome.Success -> {
+                            val loqs = outcome.data
+                            val activityOnTop = applicationsRepository.getForegroundApp()
+                            if (loqs.isLocked(activityOnTop)){
+                                val dialogIntent = Intent(service, LockScreenActivity::class.java)
+                                dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                service.startActivity(dialogIntent)
                             }
                         }
                     }
-                    .attachLifecycle(owner)
-        }, 1000, 10000, TimeUnit.MILLISECONDS)
+                }
+                .attachLifecycle(owner)
     }
 
     override fun onDestroy() {
