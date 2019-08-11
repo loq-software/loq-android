dc17cf53a5a055a31ae02b53ddb4a4ddfd15d481
DerrickRocha <drocha616@gmail.com>
2019-08-11 12:21:24 -0600

    Using rxjava for loading screen.

--- app/src/main/java/com/easystreetinteractive/loq/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/easystreetinteractive/loq/ui/viewmodels/MainViewModel.kt
@@ -1,25 +1,28 @@
 package com.easystreetinteractive.loq.ui.viewmodels
 
-import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.easystreetinteractive.loq.extensions.attachLifecycle
+import com.easystreetinteractive.loq.extensions.*
 import com.google.firebase.auth.FirebaseUser
-import com.easystreetinteractive.loq.extensions.disposeOnDetach
-import com.easystreetinteractive.loq.extensions.ioToMain
-import com.easystreetinteractive.loq.extensions.subscribeForOutcome
 import com.easystreetinteractive.loq.models.BlockedApplication
 import com.easystreetinteractive.loq.network.Outcome
 import com.easystreetinteractive.loq.network.api.AuthenticationService
 import com.easystreetinteractive.loq.network.api.LoqService
 import com.easystreetinteractive.loq.utils.Event
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainViewModel(private val authenticationService: AuthenticationService, private val loqService: LoqService): ViewModel() {
 
     val onLoqsLoaded: LiveData<Event<List<BlockedApplication>>> get() = _onLoqsLoaded
     private val _onLoqsLoaded = MutableLiveData<Event<List<BlockedApplication>>>()
+
+    val onProgressUpdated: LiveData<Event<Int>> get() = _onProgressUpdate
+    private val _onProgressUpdate = MutableLiveData<Event<Int>>()
 
     val showError: LiveData<Event<String>> get() = _showError
     private val _showError = MutableLiveData<Event<String>>()
@@ -38,6 +41,25 @@ class MainViewModel(private val authenticationService: AuthenticationService, pr
                         }
                         is Outcome.Failure ->{
                             _showError.postValue(Event("Loading error"))
+                        }
+                    }
+                }
+                .attachLifecycle(owner)
+    }
+
+    fun loadProgress(owner: LifecycleOwner) {
+        var progress = 0
+        Observable.interval(0, 50, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .map {
+                    progress ++
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeForOutcome { outcome ->
+                    when(outcome){
+                        is Outcome.Success -> {
+                            _onProgressUpdate.postValue(Event(outcome.data))
                         }
                     }
                 }
