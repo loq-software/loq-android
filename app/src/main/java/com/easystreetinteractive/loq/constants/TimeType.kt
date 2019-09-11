192efbe555cdb7b3097fc50b7e09633265e5e193
Derrick Rocha <drocha616@gmail.com>
2019-09-10 19:50:29 -0600

    One time loq (#37)
    * Added OneTimeLoqFragment. Updated video ad id.
    
    * VersionCode bump.

---
+++ app/src/main/java/com/easystreetinteractive/loq/constants/TimeType.kt
@@ -0,0 +1,7 @@
+package com.easystreetinteractive.loq.constants
+
+enum class TimeType {
+
+    FOR_START,
+    FOR_END
+}
--- app/src/main/java/com/easystreetinteractive/loq/models/BlockedDay.kt
+++ app/src/main/java/com/easystreetinteractive/loq/models/BlockedDay.kt
@@ -4,4 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class BlockedDay(val dayOfWeek: String = "", var time: BlockTime? = null): Parcelable
+data class BlockedDay(
+        val dayOfWeek: String = "",// todo: Use integer for day of week instead of String
+        val dayOfMonth: Int = -1,
+        val monthOfYear: Int = -1,
+        var time: BlockTime? = null
+): Parcelable
